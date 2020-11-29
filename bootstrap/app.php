a46d9de39b7667044db9e70ea89f157939fd4b2c
Nasrul Hazim <nasrulhazim.m@gmail.com>
2020-11-30 07:11:14 +0800

    🛠️ Published Config Files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -94,6 +94,7 @@ $app->configure('app');
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(\Illuminate\Redis\RedisServiceProvider::class);
 $app->register(\Dingo\Api\Provider\LumenServiceProvider::class);
 /*
 |--------------------------------------------------------------------------
